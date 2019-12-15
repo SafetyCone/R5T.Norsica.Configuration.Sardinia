56f903cb7cd15ec954960ce5b84c3851ff5ded63
MinexAutomation <minex.automation@gmail.com>
2019-12-14 23:11:50 -0800

    Added a configuration-based INuGetExecutableFilePathProvider implementation.

---
+++ source/R5T.Norsica.Configuration.Sardinia/Code/Services/Implementations/ConfigurationBasedNuGetExecutableFilePathProvider.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+using Microsoft.Extensions.Options;
+
+
+namespace R5T.Norsica.Configuration
+{
+    public class ConfigurationBasedNuGetExecutableFilePathProvider : INugetExecutableFilePathProvider
+    {
+        private IOptions<NuGetConfiguration> NuGetConfiguration { get; }
+
+
+        public ConfigurationBasedNuGetExecutableFilePathProvider(IOptions<NuGetConfiguration> nuGetConfiguration)
+        {
+            this.NuGetConfiguration = nuGetConfiguration;
+        }
+
+        public string GetNugetExecutableFilePath()
+        {
+            var nuGetConfiguration = this.NuGetConfiguration.Value;
+
+            var nugetExecutableFilePath = nuGetConfiguration.NuGetExecutableFilePath;
+            return nugetExecutableFilePath;
+        }
+    }
+}
