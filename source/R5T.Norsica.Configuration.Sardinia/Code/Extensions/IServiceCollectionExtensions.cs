85aae95d32824276564ce6ca74d23e5bec7fee14
MinexAutomation <minex.automation@gmail.com>
2020-02-17 18:58:54 -0800

    Improved IServiceCollection extensions to use ServiceAction<T> functionality.

--- source/R5T.Norsica.Configuration.Sardinia/Code/Extensions/IServiceCollectionExtensions.cs
+++ source/R5T.Norsica.Configuration.Sardinia/Code/Extensions/IServiceCollectionExtensions.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
 
+using R5T.Dacia;
 using R5T.Sardinia;
 
 using RawNuGetConfiguration = R5T.Norsica.Configuration.Raw.DotnetConfiguration;
@@ -11,14 +13,52 @@ namespace R5T.Norsica.Configuration.Sardinia
 {
     public static class IServiceCollectionExtensions
     {
+        /// <summary>
+        /// Adds the <see cref="DotnetConfiguration"/> options service.
+        /// </summary>
         public static IServiceCollection AddDotnetConfiguration(this IServiceCollection services)
         {
             services
+                .AddOptions()
                 .Configure<RawNuGetConfiguration>()
                 .ConfigureOptions<DotnetConfigurationConfigureOptions>()
                 ;
 
             return services;
+        }
+
+        /// <summary>
+        /// Adds the <see cref="DotnetConfiguration"/> options service.
+        /// </summary>
+        public static ServiceAction<IOptions<DotnetConfiguration>> AddDotnetConfigurationAction(this IServiceCollection services)
+        {
+            var serviceAction = new ServiceAction<IOptions<DotnetConfiguration>>(() => services.AddDotnetConfiguration());
+            return serviceAction;
+        }
+
+        /// <summary>
+        /// Adds the <see cref="ConfigurationBasedDotnetExecutableFilePathProvider"/> implementation of <see cref="IDotnetExecutableFilePathProvider"/> as a <see cref="ServiceLifetime.Singleton"/>.
+        /// </summary>
+        public static IServiceCollection AddConfigurationBasedDotnetExecutableFilePathProvider(this IServiceCollection services,
+            ServiceAction<IOptions<DotnetConfiguration>> addDotnetConfiguration)
+        {
+            services
+                .AddSingleton<IDotnetExecutableFilePathProvider, ConfigurationBasedDotnetExecutableFilePathProvider>()
+                .RunServiceAction(addDotnetConfiguration)
+                ;
+
+            return services;
+        }
+
+        /// <summary>
+        /// Adds the <see cref="ConfigurationBasedDotnetExecutableFilePathProvider"/> implementation of <see cref="IDotnetExecutableFilePathProvider"/> as a <see cref="ServiceLifetime.Singleton"/>.
+        /// </summary>
+        public static ServiceAction<IDotnetExecutableFilePathProvider> AddConfigurationBasedDotnetExecutableFilePathProviderAction(this IServiceCollection services,
+            ServiceAction<IOptions<DotnetConfiguration>> addDotnetConfiguration)
+        {
+            var serviceAction = new ServiceAction<IDotnetExecutableFilePathProvider>(() => services.AddConfigurationBasedDotnetExecutableFilePathProvider(
+                addDotnetConfiguration));
+            return serviceAction;
         }
     }
 }
